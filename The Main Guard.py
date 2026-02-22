c9950554021c8b4f7c850f61f8109850a9cd995e
JadedJackel <JadedJackel@outlook.com>
2026-02-22 15:32:12 -0500

    An example of the "Main Guard" script
    An example of the "Main Guard" script, and an example of how we can "pause" our program with an input statement, and a few additional notes.

---
+++ The Main Guard.py
@@ -0,0 +1,43 @@
+'''================================================================='''
+#                  <====|--  Jaded $ Jackel  --|====>                 #
+'''=================================================================>>>
+Date:       02-22-2026                                                  
+Project:    Just the Basics - Practice with if --main-- ==
+Notes:      Script comes from WGU, foundations of coding section 6
+<<<================================================================='''
+#--------|--------|----------------|--------------------------------!64
+
+
+def main():
+
+    print("Hello World")
+
+    # This is a neat way to pause the script
+    input("Press Enter to exit")
+
+
+# This call of the main function is within the >if __main__ == "__main__":< block, ensuring it's 
+# only executed when the script is run directly
+if __name__ == "__main__":
+    main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
